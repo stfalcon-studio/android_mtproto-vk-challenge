ee974acdbe2b0ed7662ef7c84c0bbed38d95cebf
Aleksandr Zozulja <alexz89ua@gmail.com>
2013-07-22 11:06:18 +0300

    buttons

--- android/src/com/stfalcon/mtpclient/MainActivity.java
+++ android/src/com/stfalcon/mtpclient/MainActivity.java
@@ -1,19 +1,41 @@
 package com.stfalcon.mtpclient;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
 
 
 public class MainActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-
+        setContentView(R.layout.activity_main);
 		super.onCreate(savedInstanceState);
     }
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		getMenuInflater().inflate(R.menu.main, menu);
+        Button startService = (Button) findViewById(R.id.start);
+        Button stopService = (Button) findViewById(R.id.stop);
+
+        startService.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        stopService.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
 		return true;
 	}
 
